f7b6cf2b76a4179c623efbb46f33a64acccda435
Guin Kellam <guin@sport-travel.com>
2019-06-03 23:52:17 -0400

    deploy

---
+++ precache-manifest.839dfaa63fdbfbfd814118c43ee7e067.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "ddd9f4b3fb35ccd987b2f3bc4c337402",
+    "url": "/turntracker/service-worker.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/turntracker/robots.txt"
+  },
+  {
+    "revision": "6c1d66c66ae96af553bb",
+    "url": "/turntracker/js/settings.93a3fd72.js"
+  },
+  {
+    "revision": "cad34ef3c1484000b2d3",
+    "url": "/turntracker/js/chunk-vendors.3811d684.js"
+  },
+  {
+    "revision": "55a1b764cc081e731903",
+    "url": "/turntracker/js/app.2937ec00.js"
+  },
+  {
+    "revision": "25b087d1ba2ec1a9b101ef23e43777c5",
+    "url": "/turntracker/index.html"
+  },
+  {
+    "revision": "c01515b9a0d7d81fd1af78ad16de9a6a",
+    "url": "/turntracker/img/sigmar.c01515b9.png"
+  },
+  {
+    "revision": "a5b303538b48999d5733a8decfd160a0",
+    "url": "/turntracker/img/chaos.a5b30353.png"
+  },
+  {
+    "revision": "cad34ef3c1484000b2d3",
+    "url": "/turntracker/css/chunk-vendors.f6948b99.css"
+  },
+  {
+    "revision": "55a1b764cc081e731903",
+    "url": "/turntracker/css/app.88b6dbb4.css"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,4 @@
+importScripts("/turntracker/precache-manifest.839dfaa63fdbfbfd814118c43ee7e067.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+console.log('Hello from service-worker.js');
+
